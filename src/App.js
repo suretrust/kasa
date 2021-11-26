379df0bce6a1b04d021c028f6e9cd6d7ba4b5e02
Saheed Oladele <sdrfngg640@gmail.com>
2021-11-26 14:43:53 +0100

    feat: add routes and paths data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Navigate
+} from 'react-router-dom'
 
-function App() {
+import './App.css'
+import GroupDetails from './containers/GroupDetails'
+import Groups from './containers/Groups'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Groups />} />
+        <Route path='/groups/:id' element={<GroupDetails />} />
+        <Route path='*' element={<Navigate replace to='/' />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import App from './App'
+import MockTheme from './testUtils/mockTheme'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Alert component', () => {
+  it('renders kasa in the app', () => {
+    render(
+      <MockTheme>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </MockTheme>
+    )
+
+    const kasa = screen.getByText(/kasa/i)
+    expect(kasa).toBeInTheDocument()
+  })
+})
